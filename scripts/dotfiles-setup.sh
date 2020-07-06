9466caae0bcdbc8fa4a881613c3f116fecbeb0a4
Giseong Bak <giseong.bak@gmail.com>
2020-07-06 17:19:15 +0900

    20200706

--- scripts/dotfiles-setup.sh
+++ scripts/dotfiles-setup.sh
@@ -12,35 +12,35 @@ else
     # required packages
     # sudo apt install git build-essential fontconfig ttf-mscorefonts-installer pandoc neovim -y
 
-    DOTFILES_PATH=~/.dotfiles
-    cd $DOTFILES_PATH
+    DOTFILES_PATH=.dotfiles
+    cd ~/$DOTFILES_PATH
     git submodule update --init
     cd $HOME
 
     # zsh
-    link $DOTFILES_PATH/zshrc ~/.zshrc
-    link $DOTFILES_PATH/antigen ~/.antigen
-    link $DOTFILES_PATH/antigenrc ~/.antigenrc
+    link $DOTFILES_PATH/zshrc .zshrc
+    link $DOTFILES_PATH/antigen .antigen
+    link $DOTFILES_PATH/antigenrc .antigenrc
 
     # git
-    link $DOTFILES_PATH/gitignore ~/.gitignore
-    link $DOTFILES_PATH/gitattributes ~/.gitattributes
+    link $DOTFILES_PATH/gitignore .gitignore
+    link $DOTFILES_PATH/gitattributes .gitattributes
 
     # vim
     curl -sLf https://spacevim.org/install.sh | bash
-    link $DOTFILES_PATH/SpaceVim.d ~/.SpaceVim.d
+    link $DOTFILES_PATH/SpaceVim.d .SpaceVim.d
 
     # tmux
     rm -rf ~/.tmux
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    link $DOTFILES_PATH/tmux.conf ~/.tmux.conf
+    git clone https://github.com/tmux-plugins/tpm .tmux/plugins/tpm
+    link $DOTFILES_PATH/tmux.conf .tmux.conf
 
     # pandoc
-    link $DOTFILES_PATH/pandoc ~/.pandoc
+    link $DOTFILES_PATH/pandoc .pandoc
 
     # Misc
-    link $DOTFILES_PATH/globalrc ~/.globalrc
-    link $DOTFILES_PATH/gitignore ~/.agignore
-    link $DOTFILES_PATH/editorconfig ~/.editorconfig
-    link $DOTFILES_PATH/tldrrc ~/.tldrrc
+    link $DOTFILES_PATH/globalrc .globalrc
+    link $DOTFILES_PATH/gitignore .agignore
+    link $DOTFILES_PATH/editorconfig .editorconfig
+    link $DOTFILES_PATH/tldrrc .tldrrc
 fi
--- zshrc
+++ zshrc
@@ -50,6 +50,10 @@ alias zless=$PAGER
 [[ -d $HOME/.local/bin ]] && PATH=$PATH:$HOME/.local/bin
 [[ -d $HOME/.SpaceVim/bin ]] && PATH=$PATH:$HOME/.SpaceVim/bin
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
 # Misc
 export GTAGSLABEL=pygments
 
